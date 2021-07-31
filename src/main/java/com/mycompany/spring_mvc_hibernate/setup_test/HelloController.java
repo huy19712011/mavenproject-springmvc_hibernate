9777dfc892ce832f25fbdf0699eb29b7e224fafc
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-31 21:52:44 +0700

    Spring MVC Hibernate: Set up project - Tut 3 C16

---
+++ src/main/java/com/mycompany/spring_mvc_hibernate/setup_test/HelloController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spring_mvc_hibernate.setup_test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author huynq
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : hello
+    Created on : Jul 24, 2021, 11:26:34 AM
+    Author     : huynq
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hello</title>
+    </head>
+    <body>
+        <h1>Welcome to Spring MVC hibernate!</h1>
+    </body>
+</html>

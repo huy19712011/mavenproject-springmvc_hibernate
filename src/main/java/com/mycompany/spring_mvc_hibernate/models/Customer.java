86d49176f53c50244143ce659952455894790145
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-08-01 14:32:54 +0700

    278: Creating hibernate entity

---
+++ src/main/java/com/mycompany/spring_mvc_hibernate/models/Customer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spring_mvc_hibernate.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author huynq
+ */
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+}
